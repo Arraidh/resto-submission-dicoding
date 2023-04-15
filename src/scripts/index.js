115284151be8b0857c2d023d28dc8b825ad8dd6d
Arraidh <ari.darmika@gmail.com>
2023-04-15 17:23:35 +0700

    Added responsive navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,12 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
 
-console.log('Hello Coders! :)');
+console.log("Hello Coders! :)");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
